81ca9e7077a801be9f95299daf352c9f37a29604
farzamalam <farzamcse@gmail.com>
2021-07-03 18:32:43 +0530

    Initialized list command and explored more on completion

--- cmd/list.go
+++ cmd/list.go
@@ -1,1 +1,19 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list is used to print all the alias and commands",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list is called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
