bbc413f0a9a966787ca4186795258c4b4388f06f
farzamalam <farzamcse@gmail.com>
2021-07-15 06:43:12 +0530

    Wrote comments for Update

--- internal/hailconfig/hailconfig_test.go
+++ internal/hailconfig/hailconfig_test.go
@@ -29,3 +29,29 @@ func assertAddError(t *testing.T, got, want, key string) {
 	t.Helper()
 	t.Errorf("got: %q want: %q given: %q", got, want, key)
 }
+
+func TestUpdate(t *testing.T) {
+	hc := new(Hailconfig).WithLoader(WithMockHailconfigLoader(""))
+	hc.Parse()
+	for k, v := range scripts {
+		hc.Add(k, v)
+	}
+	want := "kubectl logs -f --tail=100"
+	hc.Update("kube-logs", want)
+	got := hc.Scripts["kube-logs"].Command
+	if got != want {
+		t.Errorf("got: %q want: %q", got, want)
+	}
+}
+
+func TestUpdateEmpty(t *testing.T) {
+	hc := new(Hailconfig).WithLoader(WithMockHailconfigLoader(""))
+	hc.Parse()
+	err := hc.Update("kube-logs", "kubectl logs -f --tail=100")
+	if err == nil {
+		t.Errorf("expected error")
+	}
+	if hc.Scripts != nil {
+		t.Errorf("should have been a nil map")
+	}
+}
