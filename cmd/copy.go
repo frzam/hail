5d91bff46b8ac90783d09df890387a36a4809a0a
farzamalam <farzamcse@gmail.com>
2021-07-22 22:02:29 +0530

    Started working on copy and move command

---
+++ cmd/copy.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"hail/internal/hailconfig"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var copyCmd = &cobra.Command{
+	Use:   "copy [alias] [new-alias]",
+	Short: "It is used to copy one command/script to a new alias.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := validateCopy(args)
+		if err != nil {
+			fmt.Printf("error in validate copy: %v\n", err)
+			os.Exit(2)
+		}
+		hc := new(hailconfig.Hailconfig).WithLoader(hailconfig.DefaultLoader)
+		defer hc.Close()
+
+		err = hc.Parse()
+		if err != nil {
+			fmt.Printf("error:%v\n", err)
+			os.Exit(2)
+		}
+		err = hc.Copy(args[0], args[1])
+		if err != nil {
+			fmt.Println("error:%v\n", err)
+			os.Exit(2)
+		}
+	},
+}
+
+func validateCopy(args []string) error {
+	if len(args) != 2 {
+		return errors.New(fmt.Sprintf("invalid number of arguments. expected 2, recieved %d", len(args)))
+	}
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(copyCmd)
+}
