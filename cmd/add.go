e82691f55ec687fe13a066ebe8b94b1f5b32869a
farzamalam <farzamcse@gmail.com>
2021-07-26 09:46:17 +0530

    Updated add cmd to take command from cmd line or from editor

--- cmd/add.go
+++ cmd/add.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"hail/internal/editor"
 	"hail/internal/hailconfig"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -13,21 +15,12 @@ var addCmd = &cobra.Command{
 	Short:     "add is used to add a new command in collection",
 	ValidArgs: []string{"alias", "command"},
 	Run: func(cmd *cobra.Command, args []string) {
-		// validating flags and args.
-		alias, err := cmd.Flags().GetString("alias")
-		command := ""
-		if err != nil || (alias == "" && len(args) < 2) {
-			checkError("error in validation", fmt.Errorf("no alias or command is present"))
-		}
+		// Get alias either from -a flag or from args.
+		alias, err := getAlias(cmd, args)
+		checkError("error in validation", err)
+
+		// Get description from -d flag
 		des, _ := cmd.Flags().GetString("description")
-		if alias == "" && len(args) > 1 {
-			alias = args[0]
-			command = strings.Join(args[1:], "")
-		} else if alias != "" && len(args) > 0 {
-			command = strings.Join(args[0:], "")
-		} else {
-			checkError("error in validation", fmt.Errorf("no alias or command is present"))
-		}
 
 		hc := new(hailconfig.Hailconfig).WithLoader(hailconfig.DefaultLoader)
 		defer hc.Close()
@@ -38,12 +31,54 @@ var addCmd = &cobra.Command{
 		if hc.IsPresent(alias) {
 			checkError("error in validation", fmt.Errorf("alias already present"))
 		}
+
+		// Get Command from arguments or from editor
+		command, err := getCommand(cmd, args)
+		checkError("error in getting cmd", err)
+
+		if alias == "" || command == "" {
+			checkError("error in validation", fmt.Errorf("no alias or command is present"))
+		}
+
 		hc.Add(alias, command, des)
 		err = hc.Save()
 		checkError("error in save", err)
 
 		success(fmt.Sprintf("command with alias '%s' has been added\n", alias))
 	},
+}
+
+func getAlias(cmd *cobra.Command, args []string) (string, error) {
+	alias, err := cmd.Flags().GetString("alias")
+	if err != nil {
+		return "", errors.Wrap(err, "error while get alias from flag")
+	}
+	if alias == "" && len(args) > 0 {
+		alias = args[0]
+	}
+	if alias == "" {
+		return alias, fmt.Errorf("no alias is found")
+	}
+	return alias, nil
+}
+
+func getCommand(cmd *cobra.Command, args []string) (string, error) {
+	command := ""
+	alias, _ := cmd.Flags().GetString("alias")
+	if len(args) == 1 && alias == "" || len(args) == 0 && alias != "" {
+		e := editor.NewDefaultEditor([]string{})
+		bCommand, _, err := e.LaunchTempFile("hail")
+		checkError("error in launching temp file", err)
+		command = string(bCommand)
+	} else if alias != "" && len(args) > 0 {
+		command = strings.Join(args[0:], "")
+	} else if alias == "" && len(args) > 1 {
+		command = strings.Join(args[1:], "")
+	}
+	if command == "" {
+		return command, fmt.Errorf("no command is found")
+	}
+	return command, nil
 }
 
 func init() {
